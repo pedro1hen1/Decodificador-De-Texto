1aacab0284e60eb9cea3c9850906a4d0ad446378
Pedro Silva <pitterller@gmail.com>
2024-03-19 22:49:37 -0300

    Atualizar estilos CSS e funcionalidades JavaScript

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,3 +29,23 @@ function funcCriptografar() {
   // Exibe a seção direita definindo seu display como "block"
   boxDireito.style.display = "block";
 }
+
+function funcDescriptografar() {
+  // Obtém o texto criptografado do elemento de texto-criptografado
+  const entrada = document.getElementById("texto-criptografado").value;
+
+  // Copia o texto criptografado para uma variável de saída
+  let saida = entrada;
+
+  // Substitui cada sequência de criptografia no texto de saída pela vogal correspondente
+  // Note que a ordem das substituições é o inverso da função de criptografia
+  saida = saida.replace(/ufat/g, "u");
+  saida = saida.replace(/ober/g, "o");
+  saida = saida.replace(/imes/g, "i");
+  saida = saida.replace(/enter/g, "e");
+  saida = saida.replace(/ai/g, "a");
+
+  // Atualiza o elemento textarea com o texto descriptografado
+  const resultadoTextarea = document.getElementById("texto-criptografado");
+  resultadoTextarea.value = saida;
+}
